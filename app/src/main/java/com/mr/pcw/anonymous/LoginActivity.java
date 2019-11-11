e8127fb06b39ec707ca91a6e1df89f88710efff4
P.C.W.D <cksdnd5433@gmail.com>
2019-11-11 12:06:22 +0900

    Initial commit

---
+++ app/src/main/java/com/mr/pcw/anonymous/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.mr.pcw.anonymous;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void clickAnonymous(View view) {
+    }
+
+    public void clickGearing(View view) {
+    }
+}
